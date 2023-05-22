bd91fb7df8f8ee58ca2d34b859de400d11a8d29f
K4-R4 <mabdman512@gmail.com>
2023-05-22 14:17:36 +0900

    feat/anchor-link: added anchor links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 import {AppBar} from "@mui/material";
 import {Toolbar} from "@mui/material";
 import Button from "@mui/material/Button";
+
+interface NavProps {
+    name: string
+}
+
+const NavButton: React.FC<NavProps> = (props: NavProps) => {
+    return (
+        <>
+            <AnchorLink
+                href={'#' + props.name}
+                style={{textDecoration: 'none', color: 'inherit'}}
+            >
+                <Button color={'inherit'} sx={{p: 1, m: 1}}>
+                    {props.name.toUpperCase()}
+                </Button>
+            </AnchorLink>
+        </>
+    )
+}
 
 const Header: React.FC = () => {
     return (
@@ -11,18 +31,22 @@ const Header: React.FC = () => {
                 style={{alignItems: 'center'}}
             >
                 <Toolbar>
-                    <Button color='inherit' sx={{p: 1, m: 1}}>
-                        ABOUT
-                    </Button>
-                    <Button color='inherit' sx={{p: 1, m: 1}}>
-                        SKILLS
-                    </Button>
-                    <Button color='inherit' sx={{p: 1, m: 1}}>
-                        Works
-                    </Button>
-                    <Button color='inherit' sx={{p: 1, m: 1}}>
-                        CONTACT
-                    </Button>
+                    <NavButton name= 'about' />
+                    <NavButton name= 'skills' />
+                    <NavButton name= 'works' />
+                    <NavButton name= 'contact' />
+                    {/*<Button color='inherit' sx={{p: 1, m: 1}}>*/}
+                    {/*    ABOUT*/}
+                    {/*</Button>*/}
+                    {/*<Button color='inherit' sx={{p: 1, m: 1}}>*/}
+                    {/*    SKILLS*/}
+                    {/*</Button>*/}
+                    {/*<Button color='inherit' sx={{p: 1, m: 1}}>*/}
+                    {/*    Works*/}
+                    {/*</Button>*/}
+                    {/*<Button color='inherit' sx={{p: 1, m: 1}}>*/}
+                    {/*    CONTACT*/}
+                    {/*</Button>*/}
                 </Toolbar>
             </AppBar>
         </>
